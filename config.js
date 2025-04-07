655ed94d08339b503ddaf582a93daa58e0c9ad84
Steven Dalbke <steven.dalbke@wonderlic.com>
2025-04-07 15:16:51 -0500

    revert pusher

--- config.js
+++ config.js
@@ -21,7 +21,6 @@ const config = {
   redirectInsecure: getBoolEnvVar('REDIRECT_INSECURE'),
   awsS3BucketName: getEnvVar('AWS_S3_BUCKET_NAME'),
   pusherKey: getEnvVar('PUSHER_KEY'),
-  pusherCluster: getEnvVar('PUSHER_CLUSTER'),
   pusherChannel: getEnvVar('PUSHER_CHANNEL'),
 };
 
